d8bba3cb084c5d4106be858856ec78dacc1b9188
aleman1990aleman <24308060610058@cetis61.edu.mx>
2025-09-24 09:08:34 -0500

    24/sep/25

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def hola_mundo():
     return '<h1>¡Hola Mundo desde Flask!</h1>'
 
+@app.route('/yami')
+def hola_mundo2():
+    return '<h1>¡yami!</h1>'
+
 if __name__ == "__main__":
     app.run(debug=True)
+    
